d0cf8dfeca097bc955ec55bef4f0ef2589001abe
sandersvendgard <sander.svendgard@icloud.com>
2026-01-16 13:57:11 +0100

    yesda

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,40 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include <ultrasonic.cpp>
+
+const int trigPin = 33;
+const int echoPin = 32;
 
 
-// put function declarations here:
-int myFunction(int, int);
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+long duration;
+float distanceCm;
+float distanceInch;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600); 
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  
+  distanceCm = duration * SOUND_SPEED/2;
+  distanceInch = distanceCm * CM_TO_INCH;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  
+  delay(500);
 }
